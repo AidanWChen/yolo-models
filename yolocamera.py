866d5bfcaedb266b0c73239804c2407473cdbde4
AidanWChen <aidanchen622@outlook.com>
2026-02-25 16:50:59 -0700

    Update yolocamera.py

--- yolocamera.py
+++ yolocamera.py
@@ -1,16 +1,21 @@
 from ultralytics import YOLO
 import cv2 as cv
 
-model = YOLO("badbottleandmalletmodel.pt")
+bottlem = YOLO("bottle.pt")
+hammerm = YOLO("hammer.pt")
+pickm = YOLO("pick.pt")
 
 capture = cv.VideoCapture(0)
 
 while True:
     ret, frame = capture.read()
 
-    results = model(frame)
+    combined_results = [ bottlem(frame), hammerm(frame),pickm(frame)]
+    
+    annotated_frame = combined_results[0][0].plot()
+    annotated_frame = combined_results[1][0].plot(img=annotated_frame)
+    annotated_frame = combined_results[2][0].plot(img=annotated_frame)
 
-    annotated_frame = results[0].plot()
     cv.imshow("YOLO Object Detection", annotated_frame) # Display the annotated frame
 
     if cv.waitKey(1) == ord('q'): # Press 'q' to quit
